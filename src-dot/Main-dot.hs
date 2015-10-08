52a6ecb2db8bf8109b521ea470caf2a252c91fe6
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-10-08 14:36:34 +0200

    create a second Main to produce graphiz output + PoC

---
+++ src-dot/Main-dot.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Data.Text.Lazy
+import Data.GraphViz
+import Data.Graph.Inductive.Example
+import Data.GraphViz.Printing
+
+main :: IO ()
+main = do
+    putStrLn $ unpack $ renderDot $ toDot $ graphToDot nonClusteredParams clr479 
